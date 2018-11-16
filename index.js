4cf74ae682cbaf25900731d0eb8d01e06f3ad5ca
857689975 <857689975@qq.com>
2018-11-16 11:01:39 +0800

    模板完成

---
+++ index.js
@@ -0,0 +1,73 @@
+module.exports = {
+	// 模版标识
+	name: 'example',
+	// 模版引擎
+	engine: 'pug',
+
+	// 页面
+	pages: [
+		{
+			name: '首页',
+			// 路径
+			path: '/',
+			// 预览图
+			thumbnail: 'home.png',
+		},
+		{
+			name: '新闻详情',
+			// 路径
+			path: '/news_show',
+			// 预览图
+			thumbnail: 'news_show.png',
+		},
+		{
+			name: '影视广告',
+			// 路径
+			path: '/movie',
+			// 预览图
+			thumbnail: 'movie.png',
+		},
+		{
+			name: '3D设计',
+			// 路径
+			path: '/stereoscopic',
+			// 预览图
+			thumbnail: 'stereoscopic.png',
+		},
+		{
+			name: '视觉设计',
+			// 路径
+			path: '/vision',
+			// 预览图
+			thumbnail: 'vision.png',
+		},
+		{
+			name: '平面设计作品',
+			// 路径
+			path: '/plane',
+			// 预览图
+			thumbnail: 'plane.png',
+		},
+		{
+			name: '最新动态',
+			// 路径
+			path: '/news',
+			// 预览图
+			thumbnail: 'news.png',
+		},
+		{
+			name: '联系我们',
+			// 路径
+			path: '/contact',
+			// 预览图
+			thumbnail: 'contact.png',
+		},
+		{
+			name: '关于我们',
+			// 路径
+			path: '/about',
+			// 预览图
+			thumbnail: 'about.png',
+		}
+	]
+}
---
+++ public/javascripts/public.js
@@ -0,0 +1,22 @@
+
+includeScript('../public/javascripts/jquery-1.9.1.min.js');
+includeScript('../public/javascripts/main.js');
+includeScript('../public/javascripts/masonry.pkgd.min.js');
+includeScript('../public/javascripts/jquery.fancybox.pack.js');
+includeScript('../public/javascripts/jquery.fancybox-buttons.js');
+includeScript('../public/javascripts/jquery.fancybox-media.js');
+includeScript('../public/javascripts/gallery.js');
+includeScript('../public/javascripts/api-v=2.0&ak=84348b36c8def15acd053822b4df9f57');
+includeScript('../public/javascripts/jquery.form.js');
+
+
+
+
+
+
+
+
+function includeScript(url) {
+	document.write('<script type="text/javascript" src="' + url + '"></script>');
+	return false;
+}
